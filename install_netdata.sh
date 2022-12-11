cb6ae55f6f4f8aa50483f6ac75d724ee9cd5bba1
Olivier Bourdon <olivier.bourdon@squarescale.com>
2022-12-11 17:58:05 +0100

    Fix Netdata installation script download location to avoid timeouts

--- install_netdata.sh
+++ install_netdata.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+NETDATA_VERSION=1.37.1
 DEST="/opt/bin"
 
-/usr/bin/sudo /usr/bin/wget --no-verbose https://my-netdata.io/kickstart-static64.sh -O ${DEST}/kickstart-static64.sh
-/usr/bin/sudo /usr/bin/bash ${DEST}/kickstart-static64.sh --non-interactive --stable-channel --dont-start-it
-/usr/bin/sudo /usr/bin/rm -rf ${DEST}/kickstart-static64.sh
+/usr/bin/sudo /usr/bin/curl -sL -o ${DEST}/kickstart.sh https://raw.githubusercontent.com/netdata/netdata/v${NETDATA_VERSION}/packaging/installer/kickstart.sh
+/usr/bin/sudo /usr/bin/bash ${DEST}/kickstart.sh --non-interactive --stable-channel --dont-start-it
+/usr/bin/sudo /usr/bin/rm -rf ${DEST}/kickstart.sh
 
 # Do not launch netdata by default
 /usr/bin/sudo /usr/bin/systemctl stop netdata

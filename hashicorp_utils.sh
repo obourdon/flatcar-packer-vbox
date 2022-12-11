37ae0f7047e1f120a2ac40eef092b510368a36ab
Olivier Bourdon <olivier.bourdon@squarescale.com>
2022-12-11 17:56:31 +0100

    Make output nicer for all HashiCorp utilities

--- hashicorp_utils.sh
+++ hashicorp_utils.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 # python3 -m http.server 8000
 # wget http://192.168.1.22:8000/script.sh && bash script.sh
@@ -9,38 +9,38 @@ CONSUL_VERSION=${CONSUL_VERSION:-1.14.2}
 NOMAD_VERSION=${NOMAD_VERSION:-1.4.3}
 LOKI_VERSION=${LOKI_VERSION:-2.7.1}
 VAULT_VERSION=${VAULT_VERSION:-1.12.2}
+CNI_VERSION=${CNI_VERSION:-1.1.1}
 
 /usr/bin/sudo /usr/bin/mkdir -p /opt/bin
 
 # TODO: parametrize linux and amd64 according to uname (-m -s)
 # TODO: factorize Hashicorp product same download and extract function
-echo -n "Installing Consul ..."
+echo -n "Installing Consul ${CONSUL_VERSION} ..."
 /usr/bin/curl -sL -o /tmp/consul.zip https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip
 /usr/bin/sudo /usr/bin/unzip -o -q -d /opt/bin /tmp/consul.zip
 /usr/bin/rm -f /tmp/consul.zip
-echo Done
+echo " Done"
 
-echo -n "Installing Nomad ..."
+echo -n "Installing Nomad ${NOMAD_VERSION} ..."
 /usr/bin/curl -sL -o /tmp/nomad.zip https://releases.hashicorp.com/nomad/${NOMAD_VERSION}/nomad_${NOMAD_VERSION}_linux_amd64.zip
 /usr/bin/sudo /usr/bin/unzip -o -q -d /opt/bin /tmp/nomad.zip
 /usr/bin/rm -f /tmp/nomad.zip
-echo Done
+echo " Done"
 
-echo -n "Installing Vault ..."
+echo -n "Installing Vault ${VAULT_VERSION} ..."
 /usr/bin/curl -sL -o /tmp/vault.zip https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip
 /usr/bin/sudo /usr/bin/unzip -o -q -d /opt/bin /tmp/vault.zip
 /usr/bin/rm -f /tmp/vault.zip
-echo Done
+echo " Done"
 
-echo "Installing CNI plugins ..."
-CNI_VERSION=1.1.1
+echo -n "Installing CNI plugins ${CNI_VERSION} ..."
 /usr/bin/curl -sL -o /tmp/cni-plugins.tgz https://github.com/containernetworking/plugins/releases/download/v${CNI_VERSION}/cni-plugins-linux-amd64-v${CNI_VERSION}.tgz
 /usr/bin/sudo /usr/bin/mkdir -p /opt/cni/bin
 /usr/bin/sudo tar -C /opt/cni/bin -xzf /tmp/cni-plugins.tgz
 /usr/bin/rm -f /tmp/cni-plugins.tgz
-echo Done
+echo " Done"
 
-echo -n "Installing Loki ..."
+echo -n "Installing Loki ${LOKI_VERSION} ..."
 /usr/bin/curl -sL -o /tmp/loki.zip https://github.com/grafana/loki/releases/download/v${LOKI_VERSION}/loki-linux-amd64.zip
 /usr/bin/sudo /usr/bin/unzip -o -q -d /opt/bin /tmp/loki.zip
 /usr/bin/sudo /usr/bin/mv /opt/bin/loki-linux-amd64 /opt/bin/loki
@@ -50,9 +50,9 @@ echo -n "Installing Loki ..."
 # https://grafana.com/docs/loki/latest/configuration/examples/
 # https://github.com/boltdb/bolt
 # https://grafana.com/blog/2020/02/19/how-loki-reduces-log-storage/
-echo Done
+echo " Done"
 
-echo -n "Installing Promtail ..."
+echo -n "Installing Promtail ${LOKI_VERSION} ..."
 /usr/bin/curl -sL -o /tmp/promtail.zip https://github.com/grafana/loki/releases/download/v${LOKI_VERSION}/promtail-linux-amd64.zip
 /usr/bin/sudo /usr/bin/unzip -o -q -d /opt/bin /tmp/promtail.zip
 /usr/bin/sudo /usr/bin/mv /opt/bin/promtail-linux-amd64 /opt/bin/promtail
@@ -61,14 +61,14 @@ echo -n "Installing Promtail ..."
 
 # From https://gitlab.com/xavki/presentations-loki-fr/-/blob/master/3-installation-promtail/slides.md
 # /etc/promtail/promtail.yml.sample
-echo Done
+echo " Done"
 
-echo -n "Installing Logcli ..."
+echo -n "Installing Logcli ${LOKI_VERSION} ..."
 /usr/bin/curl -sL -o /tmp/logcli.zip https://github.com/grafana/loki/releases/download/v${LOKI_VERSION}/logcli-linux-amd64.zip
 /usr/bin/sudo /usr/bin/unzip -o -q -d /opt/bin /tmp/logcli.zip
 /usr/bin/sudo /usr/bin/mv /opt/bin/logcli-linux-amd64 /opt/bin/logcli
 /usr/bin/rm -f /tmp/logcli.zip
-echo Done
+echo " Done"
 
 # Configure of call logcli
 # export LOKI_ADDR=http://localhost:3100 logcli
@@ -84,10 +84,10 @@ echo Done
 echo -n "Installing Docker ..."
 /usr/bin/sudo /usr/bin/systemctl enable docker
 /usr/bin/sudo /usr/bin/systemctl start docker
-echo Done
+echo " Done"
 
 echo -n "Installing cAdvisor ..."
 /usr/bin/sudo /usr/bin/mv /tmp/cadvisor /opt/bin
 /usr/bin/sudo /usr/bin/chown root:root /opt/bin/cadvisor
 /usr/bin/sudo /usr/bin/chmod 755 /opt/bin/cadvisor
-echo Done
+echo " Done"

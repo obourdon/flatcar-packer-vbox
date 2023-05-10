f4120795f30876746f591f1f44d1ec2c861cdfdc
Olivier Bourdon <olivier.bourdon@squarescale.com>
2023-05-10 18:54:49 +0200

    Force Netdata install version

--- install_netdata.sh
+++ install_netdata.sh
@@ -5,7 +5,7 @@ NETDATA_VERSION=1.39.0
 DEST="/opt/bin"
 
 /usr/bin/sudo /usr/bin/curl -sL -o ${DEST}/kickstart.sh https://raw.githubusercontent.com/netdata/netdata/v${NETDATA_VERSION}/packaging/installer/kickstart.sh
-/usr/bin/sudo /usr/bin/bash ${DEST}/kickstart.sh --non-interactive --stable-channel --dont-start-it --no-updates
+/usr/bin/sudo /usr/bin/bash ${DEST}/kickstart.sh --non-interactive --stable-channel --dont-start-it --no-updates --install-version ${NETDATA_VERSION}
 /usr/bin/sudo /usr/bin/rm -rf ${DEST}/kickstart.sh
 
 # Do not launch netdata by default
